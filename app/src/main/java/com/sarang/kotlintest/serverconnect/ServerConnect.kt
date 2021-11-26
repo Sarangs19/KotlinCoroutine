a081b107659ccfeadcdc37438c8c5791546f43fe
Sarang <sarangsarang59@gmail.com>
2021-11-26 09:30:46 +0530

    Deleted package

--- app/src/main/java/com/sarang/kotlintest/serverconnect/ServerConnect.kt
+++
@@ -1,7 +0,0 @@
-package com.sarang.kotlintest.serverconnect
-
-class ServerConnect {
-    companion object {
-        private const val BASE_URL = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast"
-    }
-}
